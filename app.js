7e6f57e3183538cd6c68afef2f359d2fec38aad6
XI Tu <tuxi1987@gmail.com>
2016-12-26 14:52:25 -0800

    revert app.js, make sure it starts

--- app.js
+++ app.js
@@ -1,63 +1,183 @@
+
+///////////////////////////////////////////////////////////////////////////
+// Module dependencies
+///////////////////////////////////////////////////////////////////////////
 var express = require('express');
+var http = require('http');
+var https = require('https');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var passport = require('passport');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var flash = require('connect-flash');
 
+global.fs = require('fs');
+
+///////////////////////////////////////////////////////////////////////////
+// Environments Settings
+///////////////////////////////////////////////////////////////////////////
 var app = express();
-
-// view engine setup
+// all environments
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+app.use(express.favicon(__dirname + '/public/images/icon_big.png'));
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.cookieParser('123456xyz'));
+app.use(express.session({cookie: { maxAge: 3*60*60*1000}})); // Session expires in SESSION_HOURS hours
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(express.methodOverride());
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.set('port', process.env.PORT || 3000);
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+///////////////////////////////////////////////////////////////////////////
+// Log4js configuration
+///////////////////////////////////////////////////////////////////////////
+var log4js = require('log4js');
+log4js.configure({
+    appenders: [
+        { type: 'console' }, //控制台输出
+        {
+            type: 'file', //文件输出
+            filename: 'logs/access.log',
+            maxLogSize: 1024*1024*100, //100MB
+            backups:3,
+            "layout": {
+                "type": "pattern",
+                "pattern": "%m"
+            },
+            "category": "AMI_HTML5"
+        }
+    ],
+    replaceConsole: true
+});
+var logger = log4js.getLogger('AMI_HTML5');
+if ('development' == app.get('env')) {
+    logger.setLevel('DEBUG');
+    app.use(log4js.connectLogger(logger, {level:log4js.levels.DEBUG, format:':method :url'}));
+} else {
+    logger.setLevel('INFO');
+    app.use(log4js.connectLogger(logger, {level:log4js.levels.INFO, format:':method :url'}));
+}
+exports.logger=logger;
+
+///////////////////////////////////////////////////////////////////////////
+// Router / Middleware configuration
+///////////////////////////////////////////////////////////////////////////
+app.use(app.router);
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+
+///////////////////////////////////////////////////////////////////////////
+// SSL Certification
+///////////////////////////////////////////////////////////////////////////
+/*var tls = require('tls');
+ var fs = require('fs');
+ var serverOptions = {
+ key: fs.readFileSync('./my_key.pem'),
+ ca:  fs.readFileSync('./intermediate.pem'),
+ cert: fs.readFileSync('./my_cert.pem')
+ };
+
+
+ //User login, need to separate from recipient login
+ function isLoggedIn(req, res, next) {
+ if (req.isAuthenticated() && ( constants.login.LOGIN_PROVIDER.FACEBOOK==req.user.provider || constants.login.LOGIN_PROVIDER.WILLGIVE==req.user.provider)) {
+ logger.debug(req.user);
+ return next();
+ } else if( req.isAuthenticated() && constants.login.LOGIN_PROVIDER.RECIPIENT==req.user.provider){
+ req.flash('error','You are logged in as a Charity. Please use your user account to login.');
+ req.logout();
+ }
+ res.redirect("/login/signin");
+ }
+ //User login, need to separate from recipient login
+ function isLoggedInAsRecipient(req, res, next) {
+ if (req.isAuthenticated() && constants.login.LOGIN_PROVIDER.RECIPIENT==req.user.provider) {
+ logger.debug(req.user);
+ return next();
+ } else if( req.isAuthenticated() && constants.login.LOGIN_PROVIDER.RECIPIENT!=req.user.provider){
+ req.flash('error','You are logged in as a user. Please use your Charity account to login.');
+ req.logout();
+ }
+ res.redirect("/recipient/login");
+ }
+
+ exports.isLoggedIn = isLoggedIn;
+ exports.isLoggedInAsRecipient = isLoggedInAsRecipient;
+
+ */
+
+
+///////////////////////////////////////////////////////////////////////////
+// Page Routing
+///////////////////////////////////////////////////////////////////////////
+app.get('/', function (req,res){
+    console.log(req.user);
+    req.session.lastPage = '/';
+
+    res.render('index',{user: req.user});
 });
 
-/*
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});*/
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/contactus', function (req,res){
+    req.session.lastPage = '/contactus';
+    res.render('contactUs', {user: req.user});
+});
+app.get('/aboutus', function (req,res){
+    req.session.lastPage = '/aboutus';
+    res.render('aboutUs', {user: req.user});
+});
+app.get('/terms', function (req,res){
+    req.session.lastPage = '/terms';
+    res.render('Terms', {user: req.user});
+});
+app.get('/Privacy', function (req,res){
+    req.session.lastPage = '/Privacy';
+    res.render('Privacy', {user: req.user});
 });
 
-module.exports = app;
 
-// Load the http module to create an http server.
-var http = require('http');
+//https://stripe.com/docs/tutorials/forms
 
 
 
-http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
+
+
+////////////////////////////////////
+//Recipient Pages / Services
+////////////////////////////////////
+
+////////////////////////////////////
+//Handle all uncaught exceptions
+////////////////////////////////////
+process.on('uncaughtException', function(err) {
+    logger.error('Caught exception: ' + err);
 });
+
+///////////////////////////////////////////////////////////////////////////
+// Start Server
+///////////////////////////////////////////////////////////////////////////
+if ('development' == app.get('env')) {
+    app.set('port', process.env.PORT || 3000);
+    http.createServer(app).listen(app.get('port'), function(){
+        logger.info('Express server listening on port ' + app.get('port'));
+    });
+}else
+{
+    app.set('port', process.env.PORT || 443);
+    https.createServer(app).listen(app.get('port'), function(){
+        logger.info('Express server listening on port ' + app.get('port'));
+    });
+    http.createServer(app).listen(80, function(){
+        logger.info('Express server listening on port 80');
+    });
+}
 
 
