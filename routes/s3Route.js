27a6b219720b50d926338125ec21ec676c143cbc
Michael <Michael@test.com>
2017-01-03 22:19:29 -0800

    s3route

--- routes/s3Route.js
+++ routes/s3Route.js
@@ -1,6 +1,36 @@
 module.exports = function(app) {
-app.get('/s3test', function (req,res){
-    res.redirect('/test.html');
-});
 
+    var AWS = require('aws-sdk');
+    var s3 = new AWS.S3();
+
+    app.get('/s3test', function (req,res){
+        res.redirect('/test.html');
+    });
+
+
+    app.post('/upload', function (req,res){
+        console.log('Start uploading the file'+req.files);
+
+        const util = require('util')
+     
+
+        req.form.complete(function (err, fields, files) {
+            // here lies your uploaded file:
+            var path = files['fileField']
+               console.log(util.inspect(files, {showHidden: false, depth: null}))
+            // do knox stuff here
+        });
+    });
 }
+
+        // var file = req.files.file;
+      
+
+
+         
+         
+
+
+
+    
+
