2c6d9b8a2a12f69e156ba0c7287b535ac90cc076
xitu <xitu@microsoft.com>
2017-01-06 20:33:46 -0800

    adding more session states table for subresponses of quicklit, tracktap and namefaces

--- src/common/xmlBuilder.js
+++ src/common/xmlBuilder.js
@@ -2,13 +2,49 @@
  * Created by xitu on 1/3/2017.
  */
 // https://www.npmjs.com/package/xmlbuilder
-var builder = require('xmlbuilder');
-var xml = builder.create('root')
-    .ele('xmlbuilder')
-    .ele('repo', {'type': 'git'}, 'git://github.com/oozcitak/xmlbuilder-js.git')
-    .end({ pretty: true});
+var xmlbuilder = require('xmlbuilder');
 
-console.log(xml);
+exports.buildSessionXml = function(session, callback) {
+    var root = xmlbuilder.begin().ele('root', {'id': session.Id, 'name':session.testName, 'startTime':session.startTime, 'endTime':session.endTime, 'status':'COMPLETED'});
+    root.ele('device')
+            .ele('uid')
+            .insertAfter('model')
+            .insertAfter('name')
+            .insertAfter('systemName')
+            .insertAfter('systemVersion')
+            .up()
+        .ele('app')
+            .ele('buildTime')
+            .up()
+        .ele('location')
+            .ele('latitude')
+            .ele('longitude')
+            .ele('altitude')
+            .up()
+        .ele('email')
+        .ele('pindex')
+    var responses = root.ele('responses');
+    for (var i = 1; i <= 3; i++) {
+        // Create an XML fragment
+        responses = xmlbuilder.create('person').att('id', i);
+        // Import the root node of the fragment after
+        // the people node of the main XML document
+        peopleNode.importDocument(person);
+    }
+    //get states from sessionstates table
+
+    var xmlString = root.end({
+        pretty: true,
+        indent: '  ',
+        newline: '\n',
+        allowEmpty: false
+    });
+    console.log(xmlString);
+
+    callback(null, xmlString);
+}
+
+
 
 /**
  <?xml version="1.0"?>
