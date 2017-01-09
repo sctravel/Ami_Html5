5380c1f4f965d032c5d048ad0b0256c9d43faf92
Michael <Michael@test.com>
2017-01-08 18:48:44 -0800

    upload file to s3 via presigned url

--- routes/s3Route.js
+++ routes/s3Route.js
@@ -1,17 +1,18 @@
 module.exports = function(app) {
-
-  
-
     app.get('/s3test', function (req,res){
-        res.redirect('/test.html');
+        res.redirect('/s3upload.html');
     });
 
 
     app.get('/getPresignedURL', function (req,res){
+        console.log("getPresignedURL")
+        var fileName = req.query.fileName;
+
+        console.log(fileName)
 
         var AWS = require('aws-sdk');
         var s3 = new AWS.S3();
-        console.log('Start getting presigned URL: '+req.files);
+        console.log('Start getting presigned URL: ', fileName);
         const util = require('util')
      
         AWS.config.update({accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY})
@@ -22,7 +23,7 @@ module.exports = function(app) {
 
         const myBucket = 'amipaces'
         //file name from post request
-        const myKey = 'items.csv'
+        const myKey = fileName
         const signedUrlExpireSeconds = 60 * 5
 
         const url = s3.getSignedUrl('getObject', {
@@ -34,8 +35,6 @@ module.exports = function(app) {
         res.send(url)
     });
 }
-
-        // var file = req.files.file;
       
 
 
