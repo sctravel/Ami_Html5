cda23c88dd01be3b1cfdc3214dbc12fe2f6cd00a
MK <C@test.com>
2017-03-24 10:35:01 -0700

    upload s3 log

--- routes/userLoginRoute.js
+++ routes/userLoginRoute.js
@@ -71,6 +71,36 @@ module.exports = function(app) {
         winston.loggers.get(req.user.sessionId).close();
         userLogin.finishSession();
         //upload zip to s3;
+        filename = 'logs/client/'+req.user.sessionId+'.log';
+        var AWS = require('aws-sdk');
+        AWS.config.update({accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY})
+        var s3 = new AWS.S3();
+        const util = require('util')
+        // Tried with and without this. Since s3 is not region-specific, I don't
+        // think it should be necessary.
+        // AWS.config.update({region: 'us-west-2'})
+        const myBucket = 'amipaces'
+        // call S3 to retrieve upload file to specified bucket
+        var uploadParams = {Bucket: myBucket, Key: '', Body: ''};
+        var file = filename;
+
+        var fileStream = fs.createReadStream(file);
+        fileStream.on('error', function(err) {
+        console.log('File Error', err);
+        });
+        uploadParams.Body = fileStream;
+
+        var path = require('path');
+        uploadParams.Key = "amipace/logs/" + req.user.sessionId + ".log";
+
+        // call S3 to retrieve upload file to specified bucket
+        s3.upload (uploadParams, function (err, data) {
+        if (err) {
+            console.log("Error", err);
+        } if (data) {
+            console.log("Upload Success", data.Location);
+        }
+        });
         req.logout();
         res.redirect("/");
     });
